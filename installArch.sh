b1bcf75af9d59050ea65591eb38247bded5f04db
liuqi <liuqi0315@gmail.com>
2017-08-10 08:55:34 +0800

    Add Error detect module

--- installArch.sh
+++ installArch.sh
@@ -2,27 +2,30 @@
 
 Continue ()
 {
-    sleep 2
     read -p "sure ?" queren
     if [ ${queren:0:1} = 'n'  ]; then
         exit 1
     fi
 }
 
-echo "please make sure you have parted and mounted already!!!"
+Error () {
+	echo "Error occured!"
+	echo "${1}"
+	exit 1
+}
+
+echo "Please make sure you have parted and mounted already!!!"
 mount | grep sda
 if [ $? -ne 0  ]; then
-	echo "please mount before!!!"
-	exit 0
+	Error "Please mount before run this script"
 fi
 
-echo "add ArchLinuxcn source to pacman's conf, and change /etc/pacman.d/mirrorlist"
+pacman_conf="/etc/pacman.conf"
+pacman_mirrorlist="/etc/pacman.d/mirrorlist"
+echo "Add ArchLinuxcn source to pacman's conf, and change /etc/pacman.d/mirrorlist"
 # pacman.conf
 cat /etc/pacman.conf | grep archlinuxcn
 if [ $? -ne 0  ]; then
-	pacman_conf="/etc/pacman.conf"
-	pacman_mirrorlist="/etc/pacman.d/mirrorlist"
-	# pacman.conf
 	echo "[archlinuxcn]" >> $pacman_conf
 	echo "SigLevel = Optional TrustAll" >> $pacman_conf
 	echo 'Server = http://mirrors.tuna.tsinghua.edu.cn/archlinuxcn/$arch' >> $pacman_conf
@@ -31,18 +34,17 @@ fi
 echo 'Server = http://mirrors.tuna.tsinghua.edu.cn/archlinux/$repo/os/$arch' > $pacman_mirrorlist
 
 
-echo update source and install base system
-echo please make sure you have a good network!!!
+echo Update source and install base system
+echo Please make sure you have a good network!!!
+Continue 
 pacman -Syy
 pacstrap /mnt base base-devel
-
 if [ $? -ne 0  ]; then
-	echo Error occured during install base system! please run script again
-	exit 1
+	Error "Error occured during install the base system, please run the script again to try to deal with this error"
 fi
 
 
-echo generate fstab and chroot
+echo Generate fstab and chroot
 genfstab -U -p /mnt >> /mnt/etc/fstab
 wget https://raw.githubusercontent.com/nxmup/AutoInstall/master/chroot.sh
 cp chroot.sh /mnt
