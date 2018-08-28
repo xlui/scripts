c892015a985aa26602a7d16622139922af9b1f4b
xlui <liuqi0315@gmail.com>
2018-08-28 13:43:09 +0800

    add shadowsocks-libev auto-install script

--- get.sh
+++ get.sh
@@ -3,9 +3,10 @@ git pull
 
 echo 'removing exist files....'
 cd ..
-rm -f arch bbr vim
+rm -f arch bbr vim ss
 
 echo 'copying new scripts....'
 cp AutoInstall/ArchLinux/arch.sh arch
 cp AutoInstall/BBR/bbr.sh bbr
 cp AutoInstall/VIM/vim.sh vim
+wget -O ss https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-libev.sh
